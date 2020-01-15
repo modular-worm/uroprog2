ed47f38cdf5fb5a5d947ccff55dfc0b9dc40f990
modular-worm <59914510+modular-worm@users.noreply.github.com>
2020-01-15 01:59:37 -0800

    commit file tugas main.c, buat output awal

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main()
+{
+    int pilihan;
+
+    printf("1. Penambahan\n");
+    printf("2. Pengurangan\n");
+    printf("3. Perkalian\n");
+    printf("4. Pembagian\n");
+    printf("5. Penurunan polinimial:\n");
+    printf("6. Multiple operation calculator (hanya bekerja dengan integer):");
+    printf("Pilih operasi (1, 2, 3, etc.):\n");
+
+    scanf("%d", &pilihan);
+
+    if
+    return 0;
+}
